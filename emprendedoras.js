783089d2ca77b1ff2d7ed5dea8d1bbeb7aa191c9
leydi-acosta96 <leydi.acosta@updigital.ac.pa>
2026-01-27 16:10:59 -0500

    Update emprendedoras.js

--- emprendedoras.js
+++ emprendedoras.js
@@ -3,15 +3,17 @@ document.addEventListener("DOMContentLoaded", () => {
   const API_URL = "https://api.sheety.co/301327363ae1c8d017800bb4566af87c/bdMr/emprendedoras";
   const form = document.getElementById("formEmprendedora");
 
-  form.addEventListener("submit", function (e) {
+  form.addEventListener("submit", e => {
     e.preventDefault();
 
     const data = {
       emprendedora: {
-        id: document.getElementById("idEmprendedora").value.trim(),
-        nombre_emprendedora: document.getElementById("nombreEmprendedora").value.trim(),
-        nombre_emprendimiento: document.getElementById("nombreEmprendimiento").value.trim(),
-        contacto: document.getElementById("contactoEmprendedora").value.trim(),
+        id: document.getElementById("idEmprendedora").value,
+        nombre_emprendedora: document.getElementById("nombreEmprendedora").value,
+        nombre_emprendimiento: document.getElementById("nombreEmprendimiento").value,
+        contacto: document.getElementById("contactoEmprendedora").value,
+        instagram: document.getElementById("instagramEmprendedora").value,
+        correo: document.getElementById("correoEmprendedora").value,
         estado: document.getElementById("estadoEmprendedora").value
       }
     };
@@ -29,7 +31,7 @@ document.addEventListener("DOMContentLoaded", () => {
       alert("Emprendedora registrada correctamente");
       form.reset();
     })
-    .catch(err => console.error("Error:", err));
+    .catch(err => console.error(err));
   });
 
 });
