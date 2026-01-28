4e00dce8687fd3910636a1c93df064f80a0329c2
leydi-acosta96 <leydi.acosta@updigital.ac.pa>
2026-01-28 12:12:46 -0500

    Update emprendedoras.js

--- emprendedoras.js
+++ emprendedoras.js
@@ -31,7 +31,7 @@ form.addEventListener("submit", function (e) {
     }
   };
 
-  // ⚠️ Reemplaza esta URL con la tuya de Sheety
+  // 
   fetch("https://api.sheety.co/301327363ae1c8d017800bb4566af87c/bdMr/emprendedoras", {
     method: "POST",
     headers: {
@@ -54,3 +54,29 @@ form.addEventListener("submit", function (e) {
 document.getElementById("btnVolver").addEventListener("click", () => {
   window.location.href = "index.html";
 });
+// cambiar estado de emprendedoras 
+
+fetch("https://api.sheety.co/301327363ae1c8d017800bb4566af87c/bdMr/emprendedoras")
+  .then(res => res.json())
+  .then(data => {
+    mostrarEmprendedoras(data.emprendedoras);
+  });
+
+function mostrarEmprendedoras(lista) {
+  const tabla = document.getElementById("tablaEmprendedoras");
+  tabla.innerHTML = "";
+
+  lista.forEach(e => {
+    tabla.innerHTML += `
+      <tr>
+        <td>${e.id}</td>
+        <td>${e.nombreEmprendedora}</td>
+        <td>${e.nombreEmprendimiento}</td>
+        <td>${e.estadoEmprendedora}</td>
+        <td>
+          <button onclick="cambiarEstado(${e.id})">Cambiar estado</button>
+        </td>
+      </tr>
+    `;
+  });
+}
