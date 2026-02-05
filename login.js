ddf2467e28e6aa5f698efa47d4961be604b20e68
leydi-acosta96 <leydi.acosta@updigital.ac.pa>
2026-02-05 10:59:49 -0500

    Update login.js

--- login.js
+++ login.js
@@ -6,19 +6,13 @@ document.getElementById("formLogin").addEventListener("submit", function (e) {
   const codigo = document.getElementById("codigoAcceso").value.trim();
 
   fetch(API_URL, {
-    method: "GET",
     headers: {
-      "Content-Type": "application/json",
       "Authorization": "Bearer mr12#"
     }
   })
-    .then(res => {
-      console.log("STATUS:", res.status);
-      if (!res.ok) throw new Error("Error HTTP");
-      return res.json();
-    })
+    .then(res => res.json())
     .then(data => {
-      console.log("DATA:", data);
+      console.log("Usuarios:", data.usuarios);
 
       const usuario = data.usuarios.find(
         u => u.codigoAcceso === codigo
@@ -31,12 +25,18 @@ document.getElementById("formLogin").addEventListener("submit", function (e) {
 
       sessionStorage.setItem("usuario", JSON.stringify(usuario));
 
-      if (usuario.rol === "admin") {
-        window.location.href = "dashboard_admin.html";
-      } else if (usuario.rol === "emprendedora") {
-        window.location.href = "dashboard_emprendedora.html";
-      } else if (usuario.rol === "vendedora") {
-        window.location.href = "dashboard_vendedora.html";
+      switch (usuario.rol) {
+        case "admin":
+          window.location.href = "dashboard_admin.html";
+          break;
+        case "emprendedora":
+          window.location.href = "dashboard_emprendedora.html";
+          break;
+        case "vendedora":
+          window.location.href = "dashboard_vendedora.html";
+          break;
+        default:
+          alert("Rol no reconocido");
       }
     })
     .catch(err => {
