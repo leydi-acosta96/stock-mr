bcc52ef2dddb5a687716bdabe059658e534c7cad
leydi-acosta96 <leydi.acosta@updigital.ac.pa>
2026-02-05 17:00:29 -0500

    Update dashboard_admin.js

--- dashboard_admin.js
+++ dashboard_admin.js
@@ -1,12 +1,15 @@
 const usuario = JSON.parse(sessionStorage.getItem("usuario"));
 
 if (!usuario || usuario.rol !== "admin") {
-  alert("Acceso no autorizado");
-  window.location.href = "login.html";
+  alert("Acceso restringido");
+  window.location.href = "index.html";
 }
 
-document.getElementById("bienvenidaAdmin").textContent =
-  `Bienvenida, ${usuario.nombre} (Administrador)`;
+document.getElementById("nombreAdmin").textContent = usuario.nombreUsuario;
+
+function ir(pagina) {
+  window.location.href = pagina;
+}
 
 function cerrarSesion() {
   sessionStorage.clear();
