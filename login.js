82b622f3f3e8adcb9876566f6ea0772963720b92
leydi-acosta96 <leydi.acosta@updigital.ac.pa>
2026-02-05 11:05:53 -0500

    Update login.js

--- login.js
+++ login.js
@@ -1,46 +1,16 @@
-const API_URL = "https://api.sheety.co/301327363ae1c8d017800bb4566af87c/bdMr/usuarios";
+document.addEventListener("DOMContentLoaded", () => {
 
-document.getElementById("formLogin").addEventListener("submit", function (e) {
-  e.preventDefault();
+  const API_URL = "https://api.sheety.co/301327363ae1c8d017800bb4566af87c/bdMr/usuarios";
 
-  const codigo = document.getElementById("codigoAcceso").value.trim();
-
-  fetch(API_URL, {
-    headers: {
-      "Authorization": "Bearer mr12#"
-    }
-  })
+  fetch(API_URL)
     .then(res => res.json())
     .then(data => {
-      console.log("Usuarios:", data.usuarios);
-
-      const usuario = data.usuarios.find(
-        u => u.codigoAcceso === codigo
-      );
-
-      if (!usuario) {
-        alert("Código inválido");
-        return;
-      }
-
-      sessionStorage.setItem("usuario", JSON.stringify(usuario));
-
-      switch (usuario.rol) {
-        case "admin":
-          window.location.href = "dashboard_admin.html";
-          break;
-        case "emprendedora":
-          window.location.href = "dashboard_emprendedora.html";
-          break;
-        case "vendedora":
-          window.location.href = "dashboard_vendedora.html";
-          break;
-        default:
-          alert("Rol no reconocido");
-      }
+      console.log("RESPUESTA COMPLETA DE SHEETY:", data);
+      alert("Mira la consola (F12)");
     })
     .catch(err => {
-      console.error(err);
-      alert("Error de conexión con el sistema");
+      console.error("ERROR FETCH:", err);
+      alert("Error de conexión");
     });
+
 });
