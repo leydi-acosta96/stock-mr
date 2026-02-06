6642f8f1a42c1aeebef67ef82afe7b038f400bba
leydi-acosta96 <leydi.acosta@updigital.ac.pa>
2026-02-05 19:22:21 -0500

    Update dashboard_admin.js

--- dashboard_admin.js
+++ dashboard_admin.js
@@ -1,31 +1,16 @@
 const usuario = JSON.parse(sessionStorage.getItem("usuario"));
 
-// 🔒 Seguridad real
-if (!usuario || usuario.rol !== "admin") {
-  alert("Acceso restringido");
+  if (!usuario || usuario.rol !== "admin") {
+    alert("Acceso restringido"); 
+    
   window.location.href = "index.html";
+  } 
+
+document.getElementById("nombreAdmin").textContent = usuario.nombreUsuario; 
+function ir(pagina) { window.location.href = pagina; 
+} 
+function cerrarSesion() { sessionStorage.clear(); 
+window.location.href = "index.html";
+
+                        
 }
-
-// Mostrar nombre del admin
-document.getElementById("nombreAdmin").textContent =
-  `👋 ${usuario.nombreUsuario}`;
-
-// 🔹 Cerrar sesión
-document.getElementById("btnLogout").addEventListener("click", () => {
-  sessionStorage.clear();
-  window.location.href = "index.html";
-});
-
-// 🔹 Navegación del menú lateral
-const botonesMenu = document.querySelectorAll(".sidebar button");
-const secciones = document.querySelectorAll(".section");
-
-botonesMenu.forEach(boton => {
-  boton.addEventListener("click", () => {
-    const target = boton.dataset.section;
-
-    secciones.forEach(sec => sec.classList.remove("active"));
-
-    document.getElementById(target).classList.add("active");
-  });
-});
